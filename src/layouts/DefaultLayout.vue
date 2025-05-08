cea1b6c19c0db1149eee6b53d2ed24dfb5234589
AON\Aon <phanthila.ch@inet.co.th>
2025-05-08 14:38:56 +0700

    ทำ router กับ default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar title="Header" />
+  <v-main>
+    <v-container>
+      <router-view/>
+    </v-container>
+  </v-main>
+  <v-footer color="indigo-lighten-1">
+    Footer
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
   <p class="test2"> Home Page </p>
+  <!-- <v-btn size="small" elevation="10" @click="test">
+    <template #append>
+      <v-icon>mdi-plus</v-icon>
+    </template>
+    add
+  </v-btn> -->
+  <p class="text-h6 ml-2">Left</p>
 </template>
 
 <script setup lang="ts">
-
+// function test() {
+//   console.log('test')
+// }
 </script>
 
 <style lang="scss" scoped>
